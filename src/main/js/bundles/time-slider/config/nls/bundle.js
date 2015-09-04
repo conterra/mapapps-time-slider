f604c197855c4ed03e7538dae45441d1ff8405cc
Lars Schneider <info@schneiderlars.de>
2015-09-04 16:15:35 +0200

    -- added apache2 license headers -- added license maven plugin

--- src/main/js/bundles/time-slider/config/nls/bundle.js
+++ src/main/js/bundles/time-slider/config/nls/bundle.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 define({root:{bundleName:"TimeSlider-Config",bundleDescription:"This bundle provides the configuration components for the time slider bundle.",menu:{title:"Time Slider"},widget:{description:"Configuration of the time slider widget",serviceId:{title:"Service ID",tooltip:"ID of service as defined in MapModel"},layerId:{title:"Layer ID",tooltip:"ID of layer as defined in MapModel"},startTime:{title:"Starting date",tooltip:"Starting date for time slider"},endTime:{title:"Ending date",tooltip:"Ending date for time slider"},
 stops:{title:"Stops",tooltip:"Stops depending on the unit, i.e. every second month"},unit:{title:"Unit",tooltip:"Unit of time slider",units:{seconds:"Seconds",minutes:"Minutes",hours:"Hours",days:"Days",weeks:"Weeks",months:"Month",years:"Years",decades:"Decades",centuries:"Centuries"}},loop:{title:"Loop",tooltip:"Start again when end is reached"},playing:{title:"Playing",tooltip:"Automatically start playing"},timeStops:{title:"Time stops",tooltip:"Time stops of slider. If left empty time stops will be calculated from start to end date with given stop number."},
 thumbMovingRate:{title:"Moving rate",tooltip:"Moving rate of animation [ms]."},thumbCount:{title:"Selector type",tooltip:"Type of selector (1 for single, 2 for range)"},datePattern:{title:"Date pattern",tooltip:"Date pattern for time slider"},excludeDataAtLeadingThumb:{title:"Exclude first data",tooltip:"Exclude data at leading thumb"},excludeDataAtTrailingThumb:{title:"Exclude last data",tooltip:"Exclude data at trailing thumb"}}},de:!0});
--- src/main/js/bundles/time-slider/config/nls/de/bundle.js
+++ src/main/js/bundles/time-slider/config/nls/de/bundle.js
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 con terra GmbH (info@conterra.de)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 define({bundleName:"TimeSlider-Konfiguration",bundleDescription:"Dieses Bundle stellt die Konfigurationskomponenten f\u00fcr das TimeSliderBundle zur Verf\u00fcgung.",menu:{title:"Time Slider"},widget:{description:"Konfiguration des Time Sliders.",serviceId:{title:"Dienste-Id",tooltip:"Id des Dienstes (wie im MapModel)"},layerId:{title:"Layer ID",tooltip:"Id des layer (wie im MapModel)"},startTime:{title:"Startdatum",tooltip:"Startdatum des Time Sliders"},endTime:{title:"Enddatum",tooltip:"Enddatum des Time Sliders"},
 stops:{title:"Unterbrechungen",tooltip:"Unterbrechungen alle x [Einheit], bspw. alle 3 Monate"},unit:{title:"Einheit",tooltip:"Einheit des Time Sliders",units:{seconds:"Sekunden",minutes:"Minuten",hours:"Stunden",days:"Tage",weeks:"Wochen",months:"Monate",years:"Jahre",decades:"Dekaden",centuries:"Jahrhunderte"}},loop:{title:"Wiederholen",tooltip:"Wiederholt die Wiedergabe"},playing:{title:"Abspielen",tooltip:"Beginnt das Abspielen automatisch"},timeStops:{title:"Skale",tooltip:"Skala des Time Sliders. Wenn nicht definiert, wird die Skala automatisch aus dem Start- und Enddatum sowie der Unterbrechungsanzahl berechnet."},
 thumbMovingRate:{title:"Animationsgeschwindigkeit",tooltip:"Geschwindigkeit der Animation [ms]."},thumbCount:{title:"Abschnitte",tooltip:"Anzahl der Abschnitte (1 f\u00fcr einfach, 2 f\u00fcr Bereich)."},datePattern:{title:"Datumsformat",tooltip:"Datumsformat der Skalenanzeige"},excludeDataAtLeadingThumb:{title:"Ersten Datensatz ignorieren",tooltip:"Ignoriert den ersten Datensatz"},excludeDataAtTrailingThumb:{title:"Letzten Datensatz ignorieren",tooltip:"Ignoriert den letzten Datensatz"}}});
