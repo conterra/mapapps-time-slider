a9fa514ff046b97aa0fc443754a3883fda78e55e
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2023-11-13 13:44:50 +0100

    Replace license

--- src/main/js/bundles/dn_timeslider/TimeSliderWidgetFactory.ts
+++ src/main/js/bundles/dn_timeslider/TimeSliderWidgetFactory.ts
@@ -1,18 +1,18 @@
-/*
- * Copyright (C) 2023 con terra GmbH (info@conterra.de)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+///
+/// Copyright (C) 2023 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
 import { InjectedReference } from "apprt-core/InjectedReference";
 import EsriDijit from "esri-widgets/EsriDijit";
 import Binding, { Binding as BindingType } from 'apprt-binding/Binding';
@@ -42,8 +42,7 @@ export default class TimeSliderWidgetFactory {
             .enable()
             .syncToLeftNow();
 
-
-            //new new without void
+        //new new without void
         return new (EsriDijit as any)(timeSliderWidget);
     }
 
