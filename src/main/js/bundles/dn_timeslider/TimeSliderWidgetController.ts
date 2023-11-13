3f7bd7b6e4ad2a170a4f7650a2aaa91f82119166
Sebastian Holtkamp <S.Holtkamp@conterra.de>
2023-11-13 13:45:49 +0100

    Refactor to TypeScript

--- src/main/js/bundles/dn_timeslider/TimeSliderWidgetController.ts
+++ src/main/js/bundles/dn_timeslider/TimeSliderWidgetController.ts
@@ -1,46 +1,52 @@
-/*
- * Copyright (C) 2023 con terra GmbH (info@conterra.de)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+///
+/// Copyright (C) 2023 con terra GmbH (info@conterra.de)
+///
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+///         http://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+///
+
+import { InjectedReference } from "apprt-core/InjectedReference";
 import TimeSlider from "esri/widgets/TimeSlider";
 import moment from "moment";
+
+import { MapWidgetModel } from "map-widget/api";
 import { TimesliderProperties } from "../../types/TimesliderProperties";
+import TimeExtent from "esri/TimeExtent";
 
 export default class TimeSliderWidgetController {
 
-    private _properties: any;
+    private _properties: InjectedReference<Record<string, any>>;
     private timeSliderWidget: any = undefined;
     private initialTimeExtent:any = undefined;
+    private _mapWidgetModel: InjectedReference<MapWidgetModel>;
 
     public activate(): void {
         const properties = this._properties;
-        this._getView().then((view: __esri.View) => {
+        this.getView().then((view: __esri.View) => {
             // check for viewTimeExtent property; if undefined don't set new view's timeExtent
             if (this._properties.viewTimeExtent) {
-                view.timeExtent = this._getViewTimeExtent(properties);
+                view.timeExtent = this.getViewTimeExtentFromConfig(properties);
             }
             this.initialTimeExtent = view.timeExtent;
         });
     }
 
     public deactivate(): void {
-        this._resetTimeExtent();
+        this.resetTimeExtent();
         this.destroyWidget();
     }
 
     public onToolActivated(): void {
-        this._getView().then((view: __esri.View) => {
+        this.getView().then((view: __esri.View) => {
             view.timeExtent = this.timeSliderWidget.timeExtent;
             if (this._properties.playOnStartup) {
                 this.timeSliderWidget.play();
@@ -50,11 +56,11 @@ export default class TimeSliderWidgetController {
 
     public onToolDeactivated(): void {
         this.timeSliderWidget.stop();
-        this._resetTimeExtent();
+        this.resetTimeExtent();
     }
 
     //new: optional param
-    public getWidget(properties?) {
+    public getWidget(properties?: InjectedReference<Record<string, any>>): TimeSlider {
         const timeSliderProperties = this.getTimeSliderProperties(properties || this._properties);
         return this.timeSliderWidget = new TimeSlider(timeSliderProperties);
     }
@@ -64,94 +70,80 @@ export default class TimeSliderWidgetController {
         this.timeSliderWidget = undefined;
     }
 
-    public getTimeSliderProperties(properties): TimesliderProperties {
+    public getTimeSliderProperties(properties: InjectedReference<Record<string, any>>): TimesliderProperties {
         const timeSliderProperties: TimesliderProperties = {
-            timeExtent: this.getInitialTimeExtent(properties),
-            fullTimeExtent: this.getFullTimeExtent(properties),
+            timeExtent: this.getInitialTimeExtentFromConfig(properties),
+            fullTimeExtent: this.getFullTimeExtentFromConfig(properties),
+            // viewTimeExtent: this.getViewTimeExtentFromConfig(properties),
+            stops: properties.stops,
             mode: properties.mode,
             loop: properties.loop,
             playRate: properties.playRate,
+            playOnStartup: properties.playOnStartup,
             timeVisible: properties.timeVisible
         };
-        const stops = this._getStops(properties);
+        const stops = this.getStops(properties);
         if (stops) {
             timeSliderProperties.stops = stops;
         }
         return timeSliderProperties;
     }
 
-    private getInitialTimeExtent(properties) {
+    private getInitialTimeExtentFromConfig(properties: InjectedReference<Record<string, any>>): __esri.TimeExtent {
         const timeExtent = properties.timeExtent;
-
-        return this._getTimeExtent(timeExtent);
+        return this.getTimeExtent(timeExtent);
     }
 
-    private getFullTimeExtent(properties) {
+    private getFullTimeExtentFromConfig(properties: InjectedReference<Record<string, any>>): __esri.TimeExtent {
         const fullTimeExtent = properties.fullTimeExtent;
-
-        return this._getTimeExtent(fullTimeExtent);
+        return this.getTimeExtent(fullTimeExtent);
     }
 
-    /**
-     * Function used to access viewTimeExtent properties and call _getTimeExtent()
-     *
-     * @returns {{start, end}} An object with start and end date
-     * @private
-     */
-    _getViewTimeExtent(properties) {
+    private getViewTimeExtentFromConfig(properties: InjectedReference<Record<string, any>>) {
         const viewTimeExtent = properties.viewTimeExtent;
-        return this._getTimeExtent(viewTimeExtent);
+        return this.getTimeExtent(viewTimeExtent);
     }
 
-    /**
-     * Function used to pass start and end component of timeExtent seperatly to _constructMoment()
-     *
-     * @param {Object} referenceTimeExtent Object representing timeExtent; Contains start and end property
-     * @returns {{start, end}} Object containing two dates, constructed using _constructMoment()
-     * @private
-     */
-    _getTimeExtent(referenceTimeExtent) {
-        const start = this._constructDate(referenceTimeExtent.start);
-        const end = this._constructDate(referenceTimeExtent.end);
-
-        return {
-            start: start,
-            end: end
-        };
+    private getTimeExtent(referenceTimeExtent: __esri.TimeExtent): __esri.TimeExtent {
+        if (moment.isDate(referenceTimeExtent.start) && moment.isDate(referenceTimeExtent.end)) {
+            return referenceTimeExtent;
+        } else {
+            return new TimeExtent({
+                start: this.constructDate(referenceTimeExtent.start),
+                end: this.constructDate(referenceTimeExtent.end)
+            });
+        }
     }
 
-    /**
-     * Function used to construct a moment from properties defined in manifest.json/app.json
-     * @param {Object} referenceMoment Moment properties extracted from referenceTimeExtent
-     * @returns Moment constructed according to parameters
-     * @private
-     */
-    _constructDate(referenceMoment) {
+    private constructDate(dateRepresentation: string | Date): Date {
+        if (!dateRepresentation) {
+            return;
+        }
+
+        if (dateRepresentation === "now") {
+            return moment().toDate();
+        }
+
         let momentObj = moment.utc();
-        // Case: Property is either "now", undefined or null
-        if (referenceMoment === "now" || !referenceMoment) {
-            // do nothing because moment object has already the current date
-        } else if (Array.isArray(referenceMoment)) {
-            referenceMoment.forEach((m, i) => {
-                // Case: Property is an array and leading element is a string
-                if (typeof m === 'string' && i === 0) {
-                    momentObj = moment(m).utc();
+        if (Array.isArray(dateRepresentation)) {
+            dateRepresentation.forEach((date, index) => {
+                if (typeof date === 'string' && index === 0) {
+                    momentObj = moment(date).utc();
                     if (momentObj && !momentObj.isValid()) {
                         momentObj = moment().utc();
                         console.warn("Invalid timeExtent definition. Use current time.");
                     }
                 } else {
                     try {
-                        momentObj[m.method].apply(momentObj, m.args);
+                        momentObj[date.method](...date.args);
                     } catch {
                         console.warn("Invalid moment definition in timeExtent definition. Use current time.");
                     }
                 }
             });
         }
-        // Case: Property is a string but not "now"
-        else if (typeof referenceMoment === 'string' && referenceMoment !== "now") {
-            momentObj = moment(referenceMoment).utc();
+        else if (typeof dateRepresentation === 'string') {
+            momentObj = moment(dateRepresentation).utc();
             if (momentObj && !momentObj.isValid()) {
                 momentObj = moment.utc();
                 console.warn("Invalid timeExtent definition. Use current time.");
@@ -161,148 +153,118 @@ export default class TimeSliderWidgetController {
         return momentObj.toDate();
     }
 
-    /**
-     * Generates stops values for the TimeSlider
-     *
-     * @returns Object that contains the stop configuration
-     * @private
-     */
-    _getStops(properties) {
+    private getStops(properties: InjectedReference<Record<string, any>>): __esri.StopsByDates |
+    __esri.StopsByCount | __esri.StopsByInterval | undefined {
         const stopsProperties = properties.stops;
         const defaultStopCount = 10;
         let stops = null;
 
-        // If stop properties are defined
-        if (stopsProperties) {
-            // Case: Stops are defined using dates
-            if (stopsProperties.dates) {
-                stops = {};
-                const dates = [];
-                let momentObj;
-                stopsProperties.dates.forEach((dateString) => {
-                    momentObj = moment(dateString).utc();
-                    // Push valid dates
+        if (!stopsProperties) {
+            return;
+        }
+
+        if (stopsProperties && !Object.keys(stopsProperties).length){
+            stops = {};
+            stops.count = defaultStopCount;
+        }
+
+        if (stopsProperties.dates) {
+            stops = {};
+            const dates = [];
+            let momentObj: moment;
+            stopsProperties.dates.forEach((dateString: string) => {
+                momentObj = moment(dateString).utc();
+                if (momentObj?.isValid()) {
+                    dates.push(momentObj.toDate());
+                }
+                else {
+                    console.warn("Invalid date stop definition. Skip value.");
+                }
+            });
+            stops.dates = dates;
+        }
+        else if (stopsProperties.count) {
+            stops = {};
+            stops.count = stopsProperties.count || defaultStopCount;
+        }
+        else if (stopsProperties.interval) {
+            stops = {};
+            stops.interval = {
+                value: stopsProperties.interval.value || 1,
+                unit: stopsProperties.interval.unit || "years"
+            };
+        }
+        else if (stopsProperties.moment) {
+            stops = {};
+            const dates = [];
+            let momentObj = moment().utc();
+
+            stopsProperties.moment.forEach((timeStop: string | Array<string> | moment) => {
+                if (typeof timeStop === 'string') {
+                    momentObj = moment(timeStop).utc();
                     if (momentObj?.isValid()) {
                         dates.push(momentObj.toDate());
+                    } else {
+                        momentObj = moment.utc();
+                        console.warn("Invalid stop definition at start of the array. Use current time.");
                     }
-                    // Warn for invalid dates and skip date
-                    else {
-                        console.warn("Invalid date stop definition. Skip value.");
-                    }
-                });
-                stops.dates = dates;
-            }
-            // Check if stops are defined using moments and calculations
-            else if (stopsProperties.moment) {
-                stops = {};
-                const dates = [];
-                let momentObj = moment().utc();
-                stopsProperties.moment.forEach((timeStop) => {
-                    // Check if leading timeStop element is a string
-                    if (typeof timeStop === 'string') {
-                        momentObj = moment(timeStop).utc();
-                        if (momentObj?.isValid()) {
-                            dates.push(momentObj.toDate());
-                        } else {
-                            momentObj = moment.utc();
-                            console.warn("Invalid stop definition at start of the array. Use current time.");
-                        }
-                    }
-                    // stop is an array
-                    else if (Array.isArray(timeStop)) {
-                        timeStop.forEach((time) => {
-                            try {
-                                momentObj[time.method].apply(momentObj, time.args);
-                                if (momentObj?.isValid()) {
-                                    dates.push(momentObj.toDate());
-                                }
-                            } catch {
-                                console.warn("Invalid stop definition in moment array. Skip array entry.");
-                            }
-                        });
-                    }
-                    // stop is a single object
-                    else {
+                }
+                else if (Array.isArray(timeStop)) {
+                    timeStop.forEach((time) => {
                         try {
-                            momentObj[timeStop.method].apply(momentObj, timeStop.args);
+                            momentObj[time.method](...time.args);
                             if (momentObj?.isValid()) {
                                 dates.push(momentObj.toDate());
                             }
                         } catch {
                             console.warn("Invalid stop definition in moment array. Skip array entry.");
                         }
-                    }
-                });
-                stops.dates = dates;
-            }
-            // Case: Stops are defined by count or interval
-            else {
-                // Case: Stops defined by count
-                if (stopsProperties.count) {
-                    stops = {};
-                    stops.count = stopsProperties.count || defaultStopCount;
+                    });
                 }
-                // Case: Stops defined by interval
-                else if (stopsProperties.interval) {
-                    stops = {};
-                    stops.interval = {
-                        value: stopsProperties.interval.value || 1,
-                        unit: stopsProperties.interval.unit || "years"
-                    };
-                }
-                // Case: No definition provided. Use 10 stops instead
                 else {
-                    stops = {};
-                    stops.count = defaultStopCount;
+                    try {
+                        momentObj[timeStop.method](...timeStop.args);
+                        if (momentObj?.isValid()) {
+                            dates.push(momentObj.toDate());
+                        }
+                    } catch {
+                        console.warn("Invalid stop definition in moment array. Skip array entry.");
+                    }
                 }
-                // Case: Stops defined by start and end time and interval/count
-                if (stopsProperties.timeExtent && stops) {
-                    let start = null;
-                    let end = null;
-                    if (stopsProperties.timeExtent.start) {
-                        start = moment(stopsProperties.timeExtent.start);
-                        if (!start?.isValid()) {
-                            start = moment.utc();
-                            console.warn("No valid configuration for stop timeExtent start. Using current time.");
-                        }
+            });
+            stops.dates = dates;
+        }
+        else {
+            if (stopsProperties.timeExtent && stops) {
+                let start = null;
+                let end = null;
+                if (stopsProperties.timeExtent.start) {
+                    start = moment(stopsProperties.timeExtent.start);
+                    if (!start?.isValid()) {
+                        start = moment.utc();
+                        console.warn("No valid configuration for stop timeExtent start. Using current time.");
                     }
-                    if (stopsProperties.timeExtent.end) {
-                        end = moment(stopsProperties.timeExtent.end);
-                        if (end && end.isValid() === false) {
-                            end = moment.utc();
-                            console.warn("No valid configuration for stop timeExtent end. Using current time.");
-                        }
+                }
+                if (stopsProperties.timeExtent.end) {
+                    end = moment(stopsProperties.timeExtent.end);
+                    if (end && end.isValid() === false) {
+                        end = moment.utc();
+                        console.warn("No valid configuration for stop timeExtent end. Using current time.");
                     }
-                    if (start && end) {
-                        stops.timeExtent = {
-                            start: start.toDate(),
-                            end: end.toDate()
-                        };
-                    }
+                }
+                if (start && end) {
+                    stops.timeExtent = {
+                        start: start.toDate(),
+                        end: end.toDate()
+                    };
                 }
             }
         }
+
         return stops;
     }
 
-    /**
-     * Converts a date string to a Date Object via moment
-     *
-     * @param config Date string
-     * @returns {Date}
-     * @private
-     */
-    _getDate(config) {
-        return moment(config).toDate();
-    }
-
-    /**
-     * Returns the current View of the MapWidgetModel
-     *
-     * @returns {Promise<View>} The current View
-     * @private
-     */
-    _getView() {
+    private getView(): Promise<__esri.View> {
         const mapWidgetModel = this._mapWidgetModel;
         return new Promise((resolve) => {
             if (mapWidgetModel.view) {
@@ -316,13 +278,8 @@ export default class TimeSliderWidgetController {
         });
     }
 
-    /**
-     * Resets the initial TimeExtent value of the View
-     *
-     * @private
-     */
-    _resetTimeExtent() {
-        this._getView().then((view) => {
+    private resetTimeExtent(): void {
+        this.getView().then((view: __esri.View) => {
             view.timeExtent = this.initialTimeExtent;
         });
     }
